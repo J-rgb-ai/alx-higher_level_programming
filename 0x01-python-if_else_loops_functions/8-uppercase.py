f5ee36c92679f5502382b47b933c2d1ddaaa9171
J-rgb-ai <opiyojoyshaney.com>
2024-01-07 02:40:52 -0800

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    def uppercase(str):
+        for c in str:
+            upper_char = chr(ord(c) - 32) if 'a' <= c <= 'z' else c
+            print("{}".format(upper_char), end='')
+            print()
