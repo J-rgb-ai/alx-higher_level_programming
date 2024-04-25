b4057bc3e1c2c9293c8d7b5ced8ff4f003774981
J-rgb-ai <opiyojoyshaney.com>
2024-04-25 03:51:51 -0700

    simple add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
-from add_0 import add
-
+#!/usr/bin/python3
 if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
 
     result = add(a, b)
 
-    print("{} + {} = {}".format(a, b, result))
+    print("{:d} + {:d} = {:d}".format(a, b, result))
